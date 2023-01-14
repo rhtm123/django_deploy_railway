62c9c9682526f2668933a7613be7d6d8a49ed9ba
rhtm123 <maurya.iitk@gmail.com>
2023-01-14 12:21:07 +0530

    postgre settings updated

--- mysite/settings_rail.py
+++ mysite/settings_rail.py
@@ -6,3 +6,16 @@ from decouple import config
 SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ['web-production-a6b8.up.railway.app']
+
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DATABASE_NAME'),
+            'USER': config('DATABASE_USER'),
+            'PASSWORD': config('DATABASE_PASSWORD'),
+            'HOST': config('DATABASE_HOST'),
+            'PORT': config('DATABASE_PORT'),
+            'OPTIONS': {'sslmode': 'require'},
+        }
+}
